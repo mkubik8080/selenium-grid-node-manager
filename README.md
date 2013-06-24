4229198c79d00479cc6fb7128f7db3ffd24152de
Michal Kubik <michal.kubik@sabre.com>
2013-06-24 16:20:19 +0200

    some changes

--- README.md
+++ README.md
@@ -1,10 +1,40 @@
 selenium-grid-node-manager
-=====================
+==========================
 
-Tool to allow remote management of selenium grid machines.
+Short description
+-----------------
+XML-RPC based tool to allow remote management of selenium grid machines.
 
 ----
-*Note: it's initial repo*
- - code is work in progress 
- - and will be pushed as soon as it will be somehow functional
- - hopefully in few days
+#### Note: *it's initial repo*
+ - code is still work in progress
+
+
+Installation:
+------
+
+Just clone repo to any folder and start manager as service or directly:
+- run manager as a Windows service or Unix daemon
+  ```bash
+  $ python node-manager.py -h
+  ```
+
+- execute manager directly
+  ```bash
+  $ python src/nodemanager.py -h
+  ```
+
+Usage:
+------
+Example usages are:
+#####Python:
+```python
+import xmlrpclib
+
+s = xmlrpclib.ServerProxy('http://localhost:5005')
+s.system.listMethods()
+```
+
+Dependencies:
+-------------
+  - running as windows service requires _python win extensions_ [__pywin32__ (win32all)](http://sourceforge.net/projects/pywin32/files/)
--- src/nodemanager.py
+++ src/nodemanager.py
@@ -5,60 +5,70 @@
 '''
 
 from SimpleXMLRPCServer import SimpleXMLRPCServer
+from optparse import OptionParser
+import shlex
 from time import localtime, strftime
 import subprocess
 import os
 import sys
 import logging
+import signal
 
 
 # basic configuration
-PORT = 5005
-LOGREQUESTS = False
 logging.basicConfig(filename='node-manager.log', level=logging.INFO)
+
+NOT_SUPPORTED = "Not supported yet, contact michal.kubik.pl@gmail.com if you need it ;)"
 
 # exit codes
 SUCCESS = True
 FAILURE = False
 
 
-class NodeManager:
+class NodeManager(SimpleXMLRPCServer):
     """
     Node Manager features served over XML-RPC protocol to be executed from remote host
     """
 
-    def __init__(self, hostName, port):
-        self.port = port
-        self.hostName = hostName
+    finished = False
 
-    def _start(self):
+    def register_signal(self, signum):
+        signal.signal(signum, self.signal_handler)
+
+    def signal_handler(self, signum, frame):
+        print "Caught signal", signum
+        self.shutdown()
+
+    def start(self):
+        self.finished = False
         try:
-            s = SimpleXMLRPCServer((self.hostName, self.port), logRequests=LOGREQUESTS)
-        except Exception, err:
-            logging.error('\tProblem starting server: %s\n' % err)
-            sys.stderr.write('Problem starting server. '
-                             'If error is: "Address already in use" wait and try again\n'
-                             '\terror is: %s\n' % err)
-            sys.exit(1)
-        s.register_instance(self)
-        s.register_introspection_functions()
-        try:
-            print '\n\tNode Manager started\n'
-            logging.info(' %s\n\tNode Manager started\n' % strftime('%d %b %Y %H:%M:%S', localtime()))
-            s.serve_forever()
+            logging.info(' %s\n\tNode Manager started' % strftime('%d %b %Y %H:%M:%S', localtime()))
+            # register breaking signals
+            # s.register_signal(signal.SIGTERM)
+            # s.register_signal(signal.SIGINT)
+
+            self.serve_forever()
         except KeyboardInterrupt:
-            print 'Exiting'
-        except:
+            logging.info('Exiting')
+        except Exception, e:
             logging.info('\tReceived term signal. Closing.')
+        return SUCCESS
 
-    def _executeCmdInShell(self, command):
-        proc = subprocess.Popen([command], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        err = proc.communicate()[1]
-        if err != '':
-            logging.warning('\tError executing shell command: %s' % err)
-            raise OSError('Error executing shell command: %s' % err)
+    def stop(self):
+        self.shutdown()
 
+    def shutdown(self):
+        self.finished = True
+        return SUCCESS
+
+    def serve_forever(self):
+        while not self.finished:
+            self.handle_request()
+
+
+class NodeManagerFunctionsBase:
     def status(self):
+        print("status")
         return SUCCESS
 
     def list_dir(self, dir_name):
@@ -68,19 +78,76 @@ class NodeManager:
         logging.debug('list_dir(%s)', dir_name)
         return os.listdir(dir_name), SUCCESS
 
-    # def startManager(self):
-    #     """Starts Node Manager with proper parameters"""
-    #
-    #     logging.info('\tStarting Node Manager...')
-    #     if (hasattr(os, "devnull")):
-    #         DEVNULL = os.devnull
-    #     else:
-    #         DEVNULL = "/dev/null"
-    #
-    #     self.devnull = open(DEVNULL, 'w')
-    #
-    #     return SUCCESS
+    def cwd(self):
+        return os.getcwd(), SUCCESS
+
+
+class NodeManagerFunctionsUnix(NodeManagerFunctionsBase):
+    def killChromedrivers(self):
+        return NOT_SUPPORTED
+
+    def killChromedrivers(self):
+        return NOT_SUPPORTED
+
+
+class NodeManagerFunctionsWin(NodeManagerFunctionsBase):
+    def killChromedrivers(self):
+        logging.info("Executing killChromedrivers request")
+        # _executeCommand("taskkill /F /IM chromedriver.exe", silently=True)
+        return _getCommandExecutionResponse("taskkill /F /IM chromedriver.exe"), SUCCESS
+        # return SUCCESS
+
+    def killChromes(self):
+        logging.info("Executing killChromes request")
+        return _getCommandExecutionResponse("taskkill /F /IM chrome.exe"), SUCCESS
+        # return SUCCESS
+
+
+def getNodeManager(host, port, logRequests=False):
+    try:
+        s = NodeManager((host, port), logRequests=logRequests)
+    except Exception, err:
+        logging.error('\tProblem starting server: %s\n' % err)
+        sys.stderr.write('Problem starting server. '
+                         'If error is: "Address already in use" wait and try again\n'
+                         '\terror is: %s\n' % err)
+        sys.exit(1)
+    if 'win' in sys.platform:
+        s.register_instance(NodeManagerFunctionsWin())
+    else:
+        s.register_instance(NodeManagerFunctionsUnix())
+
+    s.register_introspection_functions()
+
+    return s
+
+
+def _executeCommandInShell(command, silently=False):
+    devnull = open(os.devnull, 'w') if silently else None
+    return subprocess.call(shlex.split(command), shell=True, stdout=devnull, stderr=devnull)
+
+
+def _executeCommand(command, silently=False):
+    devnull = open(os.devnull, 'w') if silently else None
+    return subprocess.call(shlex.split(command), shell=False, stdout=devnull, stderr=devnull)
+
+
+def _getCommandExecutionResponse(command):
+    try:
+        output = subprocess.check_output(shlex.split(command), shell=False, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError, e:
+        output = e.output
+    logging.debug(output)
+    return output
+
 
 if __name__ == '__main__':
-    server = NodeManager('', PORT)
-    server._start()
+    parser = OptionParser()
+    parser.add_option("-P", "--port", dest="port", default=5005, type="int",
+                      help="node-manager port [default: 5005]")
+    parser.add_option("-l", "--log-requests", action="store_true", dest="logRequests", default=False,
+                      help="enables logging requests [default: False]")
+    (options, args) = parser.parse_args()
+    s = getNodeManager('', options.port, options.logRequests)
+
+    s.start()
